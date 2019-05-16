d788cd734d665b729e5423328dc679734e4fcde0
Long Liang <lliang@Earth-C-137.local>
2019-05-16 13:00:57 -0700

    removed unused pmd.utils.py. leftover from jwt.

--- pmd/utils.py
+++
@@ -1,8 +0,0 @@
-from pmd_auth.serializers import UserSerializer
-
-def pmd_auth_jwt_response_handler(token, user=None, request=None):
-  return {
-    'token': token,
-    'user': UserSerializer(user, context={'request': request}).data
-  }
-  
