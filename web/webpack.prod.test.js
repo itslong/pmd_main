f9ba00aa54cdc45a5266ef9d0e85fd04ade267ef
Long Liang <lliang@Earth-C-137.local>
2019-05-28 22:29:19 -0700

    direct webpack bundles to /web/dist.

---
+++ web/webpack.prod.test.js
@@ -0,0 +1,52 @@
+
+var path = require('path');
+var webpack = require('webpack');
+var BundleTracker = require('webpack-bundle-tracker');
+
+
+module.exports = {
+  context: __dirname,
+  mode: 'development',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/dev-server',
+    './src/index',
+  ],
+  devtool: 'inline-source-map',
+  output: {
+    path: path.resolve('./dist/bundles/'),
+    filename: '[name]-[hash].js',
+    publicPath: 'http://localhost:3000/static/web/bundles/',
+  },
+  devServer: {
+    contentBase: './dist/bundles/',
+    historyApiFallback: true,
+    hot: true,
+    port: 3000,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new BundleTracker({
+      filename: 'webpack-stats.prod.json',
+      path: path.resolve('./dist/')
+    }),
+  ],
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  }
+};
