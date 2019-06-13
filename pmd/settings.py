8a8f8f3b53493de8e77677ba554bb9e6546c2f88
Long Liang <lliang@Earth-C-137.local>
2019-06-12 17:36:12 -0700

    adjusted DEBUG settings.

--- pmd/settings.py
+++ pmd/settings.py
@@ -24,14 +24,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 try:
   from pmd import local_settings as init_settings
   KEY_VALUE = init_settings.SECRET_KEY
+  DEBUG_SETTINGS = init_settings.DEBUG
 except ImportError:
   # in prod: use elasticbeanstalk's environement vars
   KEY_VALUE = os.getenv('DJANGO_SECRET_KEY')
+  DEBUG_SETTINGS = False
 
 SECRET_KEY = KEY_VALUE
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = DEBUG_SETTINGS
 
 ALLOWED_HOSTS = ['localhost', 'pmd-dev.herokuapp.com']
 
