0e7cf7b107ab7dcd82cc368a43ff1bc8820e08cc
Long Liang <lliang@Earth-C-137.local>
2019-06-18 17:08:51 -0700

    added PartsMarkup table to admin.

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from inventory.models import Parts, Tasks, Categories, Jobs, GlobalMarkup, TagTypesChoices
+from inventory.models import Parts, Tasks, Categories, Jobs, GlobalMarkup, TagTypesChoices, PartsMarkup
 # Register your models here.
 admin.site.register(Parts)
 admin.site.register(Tasks)
@@ -7,3 +7,4 @@ admin.site.register(Categories)
 admin.site.register(Jobs)
 admin.site.register(GlobalMarkup)
 admin.site.register(TagTypesChoices)
+admin.site.register(PartsMarkup)
