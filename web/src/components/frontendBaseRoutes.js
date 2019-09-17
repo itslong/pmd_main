70705932d5ad352829f4527e1f6205f63b6e69ee
Long Liang <lliang@Earth-C-137.local>
2019-09-16 17:17:43 -0700

    temporarily changed react path.

--- web/src/components/frontendBaseRoutes.js
+++ web/src/components/frontendBaseRoutes.js
@@ -1,5 +1,7 @@
 // prod settings
-const PROD_BASE_PATH = 'https://pmd-dev.herokuapp.com/';
+// const PROD_BASE_PATH = 'https://pmd-dev.herokuapp.com/';
+// testing
+const PROD_BASE_PATH = 'https://were-doing-a-test.herokuapp.com/';
 const PROD_API_PATH = PROD_BASE_PATH + 'api/';
 
 const PATH = '/app';
